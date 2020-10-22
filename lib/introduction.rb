3a8340ba05f389d23abfe0a61c83735a80704f79
Colton O'Connor <colton.shawn.oconnor@gmail.com>
2020-10-22 02:58:17 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,7 +7,9 @@ def introduction (name)
   puts "Hit, my name is #{name}"
 end
 
-
+def introduction_with_language (name, language)
+  puts "Hi, my name is #{name} and I am learning #{language}"
+end
 
 
 
